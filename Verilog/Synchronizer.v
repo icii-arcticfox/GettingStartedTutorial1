705bd2c252c8c8710dab2aaa0439243b4582ea61
SuperMB <littlebass09@tamu.edu>
2023-03-06 22:13:51 -0800

    MB Setting initial commit

---
+++ Verilog/Synchronizer.v
@@ -0,0 +1,51 @@
+//S***Welcome to Arctic Fox!
+//***Since t_his is the first tutorial, we first present some useful information about the 
+//***look and setup of the tutorials. Please read the information below, and then proceed
+//***to Step 2. 
+
+
+
+//S***Activating Arctic Fox
+//***If you have not yet activated Arctic Fox, you must do so before Arctic Fox can run. Also,
+//***if you haven't activated Arctic Fox, all of the Special Formatting won't be here!! 
+
+//***Click the Arctic Fox logo on the left vertical bar. This will open the Arctic Fox pane on
+//***the left side of the screen. Enter your email and password and click the Activate button. 
+
+//***If you have not created an Arctic Fox account, visit the "here" link in the Arctic Fox
+//***pane and create and account. Because Arctic Fox is in alpha, after an account is created,
+//***Icii must approve the account. Please give Icii 24 hours to approve any new accounts.  
+
+
+
+//S***Special Formatting
+//***Arctic Fox embeds special formatting into Verilog code. This formatting is designed to
+//***make it more clear what is code, and what isn't. For example, the italic grey text you
+//***are reading is simply a comment whose line starts with //*** . Don't worry, any text
+//***with special formatting is a comment and will not impact your code.
+
+//***Additionally, in our tutorials, sections with start with a section header, as seen above.
+//***Section headers are underlined. Continuing on.
+
+
+
+//S***Steps
+//***Another piece of information for the tutorials, each Step of a tutorial may have substeps, 
+//***such as a, b, c, etc. The steps will be highlighted green, as you can see in the step below.
+//***To move to the next step in a tutorial, click the Arctic Fox icon on the vertical bar on the
+//***left. In the Tutorials area, click the drop down, and select the step you want to advance to.
+//***Once selected, the text below the drop down will describe the step. Click the Switch button 
+//***to move to that step in the tutorial.
+
+//***When you move to a new step in the tutorial, your progress for the current step will be 
+//***saved before moving to the new step. In the new step, you will be able to see the correct
+//***result for all previous steps. Please note, no generated code will be present at first. To
+//***see the results of any code generation, you will first need to run Arcitc Fox. 
+
+//***Completed steps will NOT be highlighted! Only steps for the current step will be
+//***highlighted green. 
+
+//_***Step 1) Proceed to Step 2
+//_***- Click the Arctic Fox icon on the vertical bar on the left. 
+//_***- In the Arctic Fox pane, click the Tutorials drop down. Click Step2_InsertAutomation.
+//_***- Click the Switch button. This will take you to Step 2. 
